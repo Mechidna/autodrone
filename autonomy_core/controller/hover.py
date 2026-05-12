c2c99fbd05cde020d7903f98e33b657e881fbf02
Paolo <paoloespejo00@gmail.com>
2026-05-12 12:50:17 -1000

    Removed unused scripts/backups Added drone state estimation script

--- autonomy_core/controller/hover.py
+++
@@ -1,6 +0,0 @@
-def get_command():
-    r = 0
-    p = 0
-    y = 90
-    t = .79
-    return r,p,y,t
---
+++ autonomy_core/estimation/__init__.py
@@ -0,0 +1,17 @@
+from autonomy_core.estimation.drone_estimation import (
+    DriftCorrectingGateEKF,
+    EstimatedState,
+    EstimatorLayer,
+    GateDetection,
+    GateLandmark,
+    OdometrySample,
+)
+
+__all__ = [
+    "DriftCorrectingGateEKF",
+    "EstimatedState",
+    "EstimatorLayer",
+    "GateDetection",
+    "GateLandmark",
+    "OdometrySample",
+]
